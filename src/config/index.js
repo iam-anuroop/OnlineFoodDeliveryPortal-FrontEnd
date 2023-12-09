d8afdffc78d2a4c587e01c5214b005025fcaf14a
iam-anuroop <anuroopku10@gmail.com>
2023-12-09 17:49:54 +0530

    stripe

---
+++ src/config/index.js
@@ -0,0 +1,1 @@
+export const API_URL = window.REACT_APP_API_URL || '';
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,54 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./CheckoutForm";
-
-function Payment() {
-    
-    const [clientSecret, setClientSecret] = useState("");
-    const [intentid, setIntentid] = useState("");
-
-    const stripePromise = loadStripe("pk_test_51OKOAtSGhzZ6PyhpxH1vM6VVWXulVZ3ZKLdvjGnqHU6ZWwyBLCnBvn4Xpj32NsHeTQHSUApXc8FFtuB4zehXSggL00hfgCcdr4");
+import { API_URL } from "../config/index";
+// import "./App.css";
 
 
-    useEffect(() => {
-        fetch("http://127.0.0.1:8000/user/payment/", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data.client_secret);
-            setClientSecret(data.client_secret);
-            setIntentid(data.intentid)
-          });
-          console.log('okay');
-      }, []);
+export default function Payment() {
+  const [message, setMessage] = useState("");
 
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
 
-      const appearance = {
-        theme: 'stripe',
-      };
-      const options = {
-        clientSecret,
-        appearance,
-      };
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
 
-console.log(clientSecret,'jjjjjjjjjjjjjjjj');
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
   return (
-    <div>
-        <h1>Payment</h1>
-        <div>
-            {clientSecret && (
-                <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm data={{'clientSecret':clientSecret,'intentid':intentid}} />
-                </Elements>
-            )}
-        </div>
-
+    <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+      <h3>Stubborn Attachments</h3>
+      <h5>$20.00</h5>
+      </div>
     </div>
-  )
+    <form action={'http://127.0.0.1:8000/user/payment/'} method="POST">
+      <button type="submit">
+        Checkout
+      </button>
+    </form>
+  </section>
+  );
 }
-
-export default Payment
