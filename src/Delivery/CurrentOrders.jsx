a34952695c4afcd6f90d07e99b6851475d1c2a99
iam-anuroop <anuroopku10@gmail.com>
2024-01-11 21:49:01 +0530

    newstyle

--- src/Delivery/CurrentOrders.jsx
+++ src/Delivery/CurrentOrders.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import axios from 'axios';
+import AuthContext from '../Context/AuthContext'
+
 
 const CurrentOrders = () => {
+  const { user,authTokens } = useContext(AuthContext)
+
+
+  const fetchCurrentOrders = async () => {
+    try{
+      const response = await axios.get('http://127.0.0.1:8000/delivery/currentorders/',
+          {
+            headers:{
+              'Content-Type':'application/json',
+              'Authorization': `Bearer ${authTokens.token.access}` 
+            },
+          });
+          console.log(response.data);
+    }catch(error){
+        console.log(error);
+    
+    }
+  }
+
+  useEffect(()=>{
+    
+    fetchCurrentOrders();
+
+  },[])
+
   return (
     <div>CurrentOrders</div>
   )
 }
+  
 
 export default CurrentOrders
